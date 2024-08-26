bd076591b9734e62be341a3ff5c27da8837d38d9
Ola Wingbrant <ola.wingbrant@scania.com>
2024-08-26 07:23:11 +0000

    Cleanup

--- src/isp_emitters.ml
+++ src/isp_emitters.ml
@@ -286,65 +286,43 @@ module Auxiliary = struct
     let eva_result = Eva.Results.eval_exp e req in
     emit_eva_result_of_term Ensures term (Eva.Results.as_ival eva_result) new_kf filling_actions
 
-  let construct_result_term_field typ fi =
-    Logic_const.term (TLval(TResult typ, TField (fi, TNoOffset))) (Ctype typ)
-
-  let rec find_offsets typ =
+  let rec find_field_offsets typ =
     match typ with
     | TComp (compinfo, _) ->
         List.flatten 
           (List.map
             (fun fieldinfo ->
-              p_debug "··· fieldinfo %s" fieldinfo.forig_name ~level:0;
-              let o = find_offsets fieldinfo.ftype in
+              let o = find_field_offsets fieldinfo.ftype in
               List.map (fun f -> Field (fieldinfo, f)) o)
             (Option.value compinfo.cfields ~default:[]))
     | _ -> [NoOffset]
 
 
-  let emit_struct_result_expression e req new_kf filling_actions =
-    match Cil.typeOf e with
+  let emit_struct_result_expression lvalue req new_kf filling_actions =
+    match Cil.typeOfLval lvalue with
     | TComp _ as styp->
-        let offsets = find_offsets styp in
-        p_debug "··· number of found offsets %i" (List.length offsets) ~level:0;
+        let (lhost, _) = lvalue in
+        let offsets = find_field_offsets styp in
+        p_debug "···· number of found offsets %i" (List.length offsets) ~level:0;
         List.iter 
           (fun o ->
-            (*let ofst = List.fold_right (fun fi a -> Field (fi, a)) o NoOffset in*)
             let term = Logic_const.term (TLval(TResult styp, Logic_utils.offset_to_term_offset o)) (Ctype styp) in
-            let eva_result = Eva.Results.eval_exp e req in
+            let eva_result = Eva.Results.eval_lval (lhost, o) req in
             emit_eva_result_of_term Ensures term (Eva.Results.as_ival eva_result) new_kf filling_actions)
           offsets
     | typ ->
-      p_debug "Non-TComp type" ~level:0;
+      p_debug "···· non-TComp type" ~level:3;
       let term = Logic_const.term (TLval(TResult typ, TNoOffset)) (Ctype typ) in
-      let eva_result = Eva.Results.eval_exp e req in
+      let eva_result = Eva.Results.eval_lval lvalue req in
       emit_eva_result_of_term Ensures term (Eva.Results.as_ival eva_result) new_kf filling_actions
-      
-  let rec emit_results_for_lvalue lvalue req new_kf filling_actions  level =
-    match Cil.typeOfLval lvalue with
-    | TComp (compinfo, _) ->
-        let (lhost, offset) = lvalue in
-        List.iter
-          (fun fieldinfo ->
-            emit_results_for_lvalue (lhost, Field (fieldinfo, offset)) req new_kf filling_actions (level+1))
-          (Option.value compinfo.cfields ~default:[])
-    | _ ->
-      begin
-        match lvalue with
-        | (Var lhost, offset) ->
-            let trm = Logic_const.term (TLval (TResult lhost.vtype, Logic_utils.offset_to_term_offset offset)) (Ctype lhost.vtype) in
-            let eva_result = Eva.Results.eval_lval lvalue req in
-            emit_eva_result_of_term Ensures trm (Eva.Results.as_ival eva_result) new_kf filling_actions;
-        | _ -> failwith "Expected 'Var' lhost when emitting results for lvalue."
-      end
-
+  
   (** Add ensures for the result (when exist) to the infered behavior contract
       of the given function.
       Todo: Factor out the pattern matching. *)
   let emit_ensures_for_results exp_opt req new_kf filling_actions =
     match exp_opt with
     | None -> ()
-    | Some ({enode = Lval (_, NoOffset); _} as e) -> emit_struct_result_expression e req new_kf filling_actions
+    | Some ({enode = Lval lvalue; _}) -> emit_struct_result_expression lvalue req new_kf filling_actions
     | Some e -> emit_simple_result_expression e req new_kf filling_actions
 
   let emit_req_for_function_parameters new_kf filling_actions =
